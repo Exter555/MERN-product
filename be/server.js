203f07c494136e9e33c95423b3504c4f9d665ee8
Exter555 <hebatsaya33@gmail.com>
2024-09-04 01:46:27 +0700

    final: ready for production

--- be/server.js
+++ be/server.js
@@ -1,15 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", productRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "fe/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "fe", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   connectDB();
---
+++ fe/src/pages/Createpage.jsx
@@ -0,0 +1,92 @@
+import {
+  Container,
+  VStack,
+  Heading,
+  Box,
+  useColorModeValue,
+  Input,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useProductStore } from "../store/product";
+
+const Createpage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: 0,
+    image: "",
+  });
+
+  const toast = useToast();
+  const { createProduct } = useProductStore();
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+      setNewProduct({ name: "", price: "", image: "" });
+    }
+  };
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} mb={8} size={"2xl"}>
+          Create New Product
+        </Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Price"
+              name="price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="image"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Button colorScheme="blue" onClick={handleAddProduct} w={"full"}>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
+};
+
+export default Createpage;
